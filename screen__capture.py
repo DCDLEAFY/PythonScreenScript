d26cc291e6e29f45e06cf86007e201f2a190fc6f
Aleaf <aallgaier@dundee.ac.uk>
2020-02-10 17:59:52 +0000

    moved code into function for possilble multi-threading
    Incase of inclusion of multithreading i moved the screen capturing into a function

--- screen__capture.py
+++ screen__capture.py
@@ -5,19 +5,27 @@ from PIL import ImageGrab
 import cv2
 import time
 
-previous_time = time.time()
 
-while(True):
-    print_screen = ImageGrab.grab(bbox=(0, 40, 800, 640)) #(bbox= x,y,width,height)
-
-    frame = cv2.cvtColor(np.array(print_screen), cv2.COLOR_BGR2RGB)
-
-    print('Seconds between frames = {}'.format(time.time()-previous_time)) #Seconds between frames
+def frame_capture():
     previous_time = time.time()
-    
-    cv2.imshow('window', frame) #Show frame in cv2 window
-    
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+    while(True):
+        #(bbox= x,y,width,height) - Grabbing section of screen
+        print_screen = ImageGrab.grab(bbox=(0, 40, 800, 640))
 
+        #Making sure the colour is the same
+        frame = cv2.cvtColor(np.array(print_screen), cv2.COLOR_BGR2RGB)
+
+        # Seconds between frames
+        print('Seconds between frames = {}'.format(time.time()-previous_time))
+        previous_time = time.time()
+        
+        # Show frame in cv2 window
+        cv2.imshow('window', frame) 
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+if __name__ == "__main__":
+    frame_capture()
+    pass
